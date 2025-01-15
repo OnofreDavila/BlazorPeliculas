0cd118550fa401026c9e82f934e55f58bfeeb105
Onofre Davila Morillo <66891673+OnofreDavila@users.noreply.github.com>
2025-01-14 21:23:33 -0300

    Add files via upload
    subida de carpeta shared

---
+++ BlazorPeliculas.Shared/Pelicula.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPeliculas.Shared
+{
+    public class Pelicula
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id {get; set;}
+
+        [Required(ErrorMessage = "Campo vacio, ingresa un nombre")]
+        public string? NombrePelicula {get; set;}
+
+        [Required(ErrorMessage = "Campo vacio, ingresa un director")]
+        public string? Director { get; set; }
+
+        [Range(50, 500, ErrorMessage = "Rango de duración incorrecto")]
+        public int Duracion { get; set; }
+        public DateOnly FechaEstreno { get; set; }
+    }
+}
